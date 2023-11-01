d1c0f8e2b27cc07fef6d654b55f6b2a8fcf3dd6d
franciscourquiza <franciscourquizalsrp@gmail.com>
2023-11-01 15:56:27 -0300

    Order Endpoints

--- e-commerce-API/Services/Interfaces/IOrderService.cs
+++ e-commerce-API/Services/Interfaces/IOrderService.cs
@@ -1,10 +1,13 @@
 ﻿using e_commerce_API.Models;
+using e_commerce_API.Data.Entities;
 
 namespace e_commerce_API.Services.Interfaces
 {
     public interface IOrderService
     {
-        void CreateOrder(OrderDto orderDto);
+        void AddOrder(Order newOrder);
+        List<Order> GetOrders();
+        List<Order> GetPendingOrders();
         Task<bool> SaveChangesAsync();
     }
 }
