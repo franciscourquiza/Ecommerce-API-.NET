a6d740864af08c5bd20f31289aaad382dd3784a7
Tomigarrcia <tomasgermangarcia@gmail.com>
2023-11-02 22:14:30 -0300

    falto guardado, pushie mal

--- e-commerce-API/Models/OrderDto.cs
+++ e-commerce-API/Models/OrderDto.cs
@@ -1,36 +1,13 @@
 ﻿using e_commerce_API.Data.Entities;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace e_commerce_API.Models
 {
     public class OrderDto
-<<<<<<< HEAD
     {
         [Required]
-        [Range(1, int.MaxValue, ErrorMessage = "El campo Id debe ser mayor que 0.")]
-        public int Id { get; set; }
-
-        [Required]
-        public Client Client { get; set; }
-
-        [Required]
-        [EmailAddress(ErrorMessage = "Por favor, ingrese una dirección de correo electrónico válida.")]
-        public string ClientEmail { get; set; }
-
-        public OrderState State { get; set; } = OrderState.pending;
-
-        [DataType(DataType.DateTime, ErrorMessage = "El campo debe ser una fecha y hora válida.")]
-        public DateTime OrderDate { get; private set; } = DateTime.Now.ToUniversalTime();
-
-        [Range(0.01, double.MaxValue, ErrorMessage = "El campo Precio debe ser mayor que 0.")]
-        public float OrderPrice { get; set; }
-
-        public List<Product> OrderedProducts { get; set; } = new List<Product>();
-
-=======
-    { 
-        public List<int> OrderedProducts { get; set; } 
->>>>>>> 38c1dbe1e712ad1a0cc4fe3b9035e981e389c4f0
+        public List<int> OrderedProducts { get; set; }
 
     }
 }
